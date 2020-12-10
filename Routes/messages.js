0c8d56844112a2318f7bed86becce87c2af2a843
shubham <shubham763116@gmail.com>
2020-12-10 19:35:30 +0530

    chatting done

--- Routes/messages.js
+++ Routes/messages.js
@@ -8,8 +8,10 @@ var authenticate = require('../authenticate');
 router.use(bodyParser.json());
 
 router.get('/messages/:senderRegNo/:receiverRegNo',authenticate.verifyUser,(req,res,next)=>{
-    console.log('shu');
-    var sql = `SELECT * FROM messages WHERE sender_reg_no='${req.params.senderRegNo}' AND receiver_reg_no='${req.params.receiverRegNo}'`;
+    // console.log('shu');
+    const sender= req.params.senderRegNo;
+    const receiver = req.params.receiverRegNo;
+    var sql = `SELECT * FROM messages WHERE (sender_reg_no='${sender}' AND receiver_reg_no ='${receiver}') OR(sender_reg_no = '${receiver}' AND receiver_reg_no = '${sender}')`;
     db.query(sql,[])
     .then(resp=>{
         res.statusCode = 200;
@@ -29,17 +31,10 @@ router.post('/messages/post',authenticate.verifyUser,(req,res,next)=>{
             req.body.message
         ])
         .then(resp=>{
-            console.log(req.user.reg_no);
-            console.log(req.body.receiverRegNo);
-            var sql = `SELECT * FROM messages WHERE sender_reg_no='${req.body.senderRegNo}' AND receiver_reg_no='${req.body.receiverRegNo}'`;
-                db.query(sql,[])
-                .then(data=>{
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    
-                    res.json(data);
-                })
-                .catch(err=>next(err));
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            
+            res.json(data);
         })
         .catch(err=>next(err));
 })
